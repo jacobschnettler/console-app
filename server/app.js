1c32fc8e301821127ea698fe5e8270031fd5bff5
jschnettler <jacobschnettler12@gmail.com>
2024-03-11 21:38:53 -0400

    Updates

--- server/app.js
+++ server/app.js
@@ -1,42 +1,42 @@
-var shortcut = process.env.shortcut;
+// var shortcut = process.env.shortcut;
 
-const { exec } = require('child_process');
+// const { exec } = require('child_process');
 
-function openAndCloseApp() {
-    // open shortcut
-    exec(`start "" "${shortcut}"`, (error, stdout, stderr) => {
-        if (error) {
-			console.error(`Error: ${error.message}`);
-			return;
-        }
+// function openAndCloseApp() {
+//     // open shortcut
+//     exec(`start "" "${shortcut}"`, (error, stdout, stderr) => {
+//         if (error) {
+// 			console.error(`Error: ${error.message}`);
+// 			return;
+//         }
 
-        if (stderr) {
-			console.error(`stderr: ${stderr}`);
-			return;
-        }
+//         if (stderr) {
+// 			console.error(`stderr: ${stderr}`);
+// 			return;
+//         }
 
-        console.log(`Console App executed successfully`);
-    });
+//         console.log(`Console App executed successfully`);
+//     });
 
-    setTimeout(() => {
-        console.log('Closing Console App process...');
+//     setTimeout(() => {
+//         console.log('Closing Console App process...');
 
-        exec('taskkill /im brave.exe /f', (error, stdout, stderr) => {
-            if (error) {
-                console.error(`Error: ${error.message}`);
-                return;
-            }
+//         exec('taskkill /im brave.exe /f', (error, stdout, stderr) => {
+//             if (error) {
+//                 console.error(`Error: ${error.message}`);
+//                 return;
+//             }
 
-            if (stderr) {
-                console.error(`stderr: ${stderr}`);
-                return;
-            }
+//             if (stderr) {
+//                 console.error(`stderr: ${stderr}`);
+//                 return;
+//             }
 
-            console.log(`Console App process closed successfully`);
+//             console.log(`Console App process closed successfully`);
             
-            openAndCloseApp();
-        });
-    }, 30 * 60000);
-}
+//             openAndCloseApp();
+//         });
+//     }, 30 * 60000);
+// }
 
-setTimeout(openAndCloseApp, 60000);
+// setTimeout(openAndCloseApp, 60000);
