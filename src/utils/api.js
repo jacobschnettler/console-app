c6407bc88caa72e6cca0696414181ddd90f90248
Jacob Schnettler <jacobschnettler12@gmail.com>
2025-02-15 21:04:20 -0500

    Updated

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  // baseURL: 'http://localhost:4080',
-  baseURL: "https://lab.jschnettler.com",
+  baseURL: "http://192.168.0.91:3000",
   headers: {
     Authorization: "Bearer " + localStorage.getItem("user-token"),
   },
